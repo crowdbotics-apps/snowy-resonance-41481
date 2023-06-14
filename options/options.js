4ac29a88c74a56de97f61f6ac7c1d44b0c87ca41
Crowdbotics <team@crowdbotics.com>
2023-06-14 16:38:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snowy-resonance-41481",
+  url: "https://snowy-resonance-41481.botics.co",
+  api: "https://snowy-resonance-41481.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
